1db312a3ce2ddfc613913b43b2deeebd91f16849
Tuan Anh <tuananh020400@gmail.com>
2022-04-26 22:38:08 +0700

    Read sensor OK

--- Code Arduino IOT/UART/Node/Node/Node.ino
+++ Code Arduino IOT/UART/Node/Node/Node.ino
@@ -2,23 +2,40 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include "Model.h"
+#include "DHT.h"
+
+#define DHTPIN 3
+#define FAN 2
+#define PUMP 4 
+#define LIGHT 8
+
+const int DHTTYPE = DHT11;  
+DHT dht(DHTPIN, DHTTYPE);
+
 
 RF24 radio(9,10); // CE, CSN
 const byte diachi[][6] = {"11111","11110"};
 
 byte receivenrf[30];
+String text = "";
+char mang[30];
 
 Garden garden1 = Garden(0,0,0,0,0,0,0);
 
 void setup() {
   Serial.begin(9600);
+  dht.begin();
+  pinMode(FAN,OUTPUT);
+  pinMode(PUMP,OUTPUT);
+  pinMode(LIGHT,OUTPUT);
+  pinMode(A0,INPUT);
   Serial.println("Start Node Module");
   NRFSetup();
 }
 
 void loop() {
-  ReadNRF ();
-
+  ReadNRF();
+  Read_Sensor();
 }
 
 void NRFSetup(){
@@ -42,8 +59,6 @@ void NRFSetup(){
 
 void ReadNRF(){
  radio.stopListening();
- String text = "A1B23.6C60.6D23E";
- char mang[30];
  text.toCharArray(mang,30);
  radio.write(&mang, sizeof(mang));
  delay(10);
@@ -54,4 +69,31 @@ void ReadNRF(){
  Serial.println((String)((char*)receivenrf));
  //XulychuoiNRF((String)((char*)receivenrf));
  delay(10);
+}
+
+void Read_DHT(){
+  garden1.setDoAm(dht.readHumidity()); 
+  garden1.setNhietDo(dht.readTemperature()) ;
+  Serial.print("Nhiet do: ");
+  Serial.println(garden1.getNhietDo());               
+  Serial.print("Do am: ");
+  Serial.println(garden1.getDoAm());
+}
+
+void Read_DAT(){
+  static float doAmDat = analogRead(A0);
+  garden1.setDoAmDat(map(doAmDat,0,1023,0,100));
+  Serial.print("Do am dat");
+  Serial.println(garden1.getDoAmDat());
+}
+
+void Read_Sensor(){
+  static int lastSensor = millis();
+  if(millis() - lastSensor >= 500){
+    Read_DAT();
+    Read_DHT();
+    text = "A1B" + (String)garden1.getNhietDo() + "C" + (String)garden1.getDoAm() + "D" + (String)garden1.getDoAmDat()+ "E";
+    //sprintf(text,"A1B%fC%fD%fE",
+    lastSensor = millis();
+  }
 }
